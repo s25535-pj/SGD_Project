a19940755e613cab4d09f89c4d4ce0b999d8baf4
paranojia <paradoxxpl@gmail.com>
2024-04-03 21:01:45 +0200

    Add bricks

--- Brick.h
+++ Brick.h
@@ -10,6 +10,13 @@ private:
     bool isVisible;
 
 public:
+    Brick() {
+        rect.x = 0;
+        rect.y = 0;
+        rect.w = 0;
+        rect.h = 0;
+        isVisible = true;
+    }
     Brick(int x, int y, int w, int h) {
         rect.x = x;
         rect.y = y;
@@ -18,11 +25,16 @@ public:
         isVisible = true;
 
     }
-
+    const SDL_Rect* getRect() const { return &rect; }
+    int getX() const { return rect.x; }
+    int getY() const { return rect.y; }
+    int getW() const { return rect.w; }
+    int getH() const { return rect.h; }
     bool getIsVisible() const { return isVisible; }
 
-    void setIsVisible(bool new_isVisible)  {
-        isVisible = new_isVisible;
+    void setIsVisible(bool value)  {
+        isVisible = value;
+        std::cout << "changed visibility" << std::endl;
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <SDL.h>
 #include <cmath>
 #include <iostream>
+#include <vector>
+#include <memory>
 #include "Player.h"
 #include "Ball.h"
 #include "Brick.h"
@@ -8,21 +10,23 @@
 #define WINDOW_WIDTH 1000
 #define WINDOW_HEIGHT 1000
 
-#define PLAYER_WIDTH 100
+#define PLAYER_WIDTH 150
 #define PLAYER_HEIGHT 12
-#define PLAYER_SPEED 15
+#define PLAYER_SPEED 10
 #define PLAYER_SPACE_FROM_BOTTOM 100
 
 #define BALL_SPEED 10
 #define BALL_SIZE 30
 
-#define MAX_BALL_SIDEWAYS_BOUNCE_FROM_PLAYER_ANGLE 3 // Piłkę odbija od ścian bocznych w sytuacji jak odbijemy krawendzą paletki.
-#define MIN_BALL_UP_BOUNCE_FROM_PLAYER_ANGLE 2       // Piłkę odbija od ścian bocznych w sytuacji jak odbijemy bokiem paletki a nie górą.
+#define MAX_BALL_SIDEWAYS_BOUNCE_OF_PLAYER 0.5 // Jak mocno piłkę może odbić w bok max: 1
 
-#define BRICK_COLUMNS 7
+#define BRICK_COLUMNS 10
 #define BRICK_ROWS 5
 #define BRICK_HEIGHT 30
-#define SPACE_BETWEEN_BRICKS 16
+#define VERTICAL_SPACE_BETWEEN_BRICKS 30
+#define HORIZONTAL_SPACE_BETWEEN_BRICKS 30
+
+#define SPACE_FROM_SCREEN_TOP 20
 
 // Tego nie tykać, wylicza się z poprzednich.
 #define PLAYER_START_POS_X (( WINDOW_WIDTH - PLAYER_WIDTH )/2 )
@@ -85,7 +89,7 @@ void handleEvents(bool &game_active, Player &player) {
         player.moveRight();
     }
 }
-void drawObjects(SDL_Renderer *renderer, Player &player, Ball &ball) {
+void drawObjects(SDL_Renderer *renderer, Player &player, Ball &ball, Brick (bricks)[][BRICK_COLUMNS]) {
 
     //Czyszczenie ekranu
     SDL_SetRenderDrawColor(renderer, 120, 125, 125, 255);
@@ -100,12 +104,33 @@ void drawObjects(SDL_Renderer *renderer, Player &player, Ball &ball) {
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderFillRect(renderer, ball.getRect());
 
+    // Narysuj cegły
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+//    for (auto& row : bricks) {
+//        for (Brick brick : row) {
+//            if (brick.getIsVisible()) {
+//                SDL_RenderFillRect(renderer, brick.getRect());
+////                std::cout << "x,y: " << brick.getX() << " " << brick.getY() << std::endl;
+////                std::cout << "w,z: " << brick.getW() << " " << brick.getH() << std::endl;
+//            }
+//        }
+////        std::cout << std::endl;
+//    }
+
+    for (int i = 0; i < BRICK_ROWS; ++i) {
+        for (int j = 0; j < BRICK_COLUMNS; ++j) {
+            if (bricks[i][j].getIsVisible()) {
+                SDL_RenderFillRect(renderer, bricks[i][j].getRect());
+            }
+        }
+    }
 
 
     // Wyświetl na ekran
     SDL_RenderPresent(renderer);
 }
-void handleCollisions(int window_width, int window_height, int maxAngleX, int maxAngleY, Player &player,  Ball &ball) {
+void handleCollisions(int window_width, int window_height, double maxSidewaysBounce, Player &player,  Ball &ball, Brick (bricks)[][BRICK_COLUMNS]) {
     // Odbicia piłki od ścian
     ////Sufit
     if (ball.getY() <= 0) {
@@ -131,43 +156,40 @@ void handleCollisions(int window_width, int window_height, int maxAngleX, int ma
     // Kolizja piłki z graczem
     if (SDL_HasIntersection(ball.getRect(), player.getRect())) {
 //        ball.flipVertically();
+        // Liczenie odległości między graczem a piłką
         double relative = player.getX() + (player.getW()/2) - (ball.getX() + ball.getSize()/2);
+        // Skalowanie od -1 do 1
         double normalized = relative / (player.getW()/2);
+
+        if (normalized > maxSidewaysBounce) {
+            normalized = maxSidewaysBounce;
+        } else if (normalized < -maxSidewaysBounce) {
+            normalized = -maxSidewaysBounce;
+        }
+//        std::cout << normalized << std::endl;
         double bounce = normalized * (5* M_PI/12);
 
-        double new_dx = ball.getSpeed() * -sin(bounce);
-        double new_dy = -ball.getSpeed() * cos(bounce);
-        double transfer_speed;
+        ball.setDx(ball.getSpeed() * -sin(bounce));
+        ball.setDy(-ball.getSpeed() * cos(bounce));
 
-        // By mi piłkę na maksa w ściany nie odbijało bo to denerwuje
-//        if (new_dx > maxAngleX) {
-////            1 = 8 - 7
-//            transfer_speed = new_dx - maxAngleX; // zmniejsz
-//            new_dx = maxAngleX;
-//        }
-//        else if (new_dx < -maxAngleX) {
-////            -1 = -8 + 7
-//            transfer_speed = -new_dx + maxAngleX;  // zwiększ
-//            new_dx = -maxAngleX;
-//        }
-//        new_dy = new_dy - transfer_speed;
-//        if (new_dy > maxAngleY) {
-//            new_dx = maxAngleY;
-//        }
-//        else if (new_dy < -maxAngleY) {
-//            new_dy = -maxAngleY;
-//        }
-        ball.setDx(new_dx);
-        ball.setDy(new_dy);
-
-        std::cout << ball.getDx() << ":" << ball.getDy() << ":" << ball.getSpeed()  << std::endl;
+//        std::cout << ball.getDx() << ":" << ball.getDy() << ":" << ball.getSpeed()  << std::endl;
     }
 
+    // Kolizja piłki z cegłami
+    for (int i = 0; i < BRICK_ROWS; ++i) {
+        for (int j = 0; j < BRICK_COLUMNS; ++j) {
+            if (bricks[i][j].getIsVisible() && SDL_HasIntersection(ball.getRect(), bricks[i][j].getRect())) {
+                std::cout << "Ball killed brick: " << bricks[i][j].getX() << " " << bricks[i][j].getY() << std::endl;
+                bricks[i][j].setIsVisible(false);
+                ball.flipVertically();
+            }
+        }
+    }
 }
 void updateObjects(Ball &ball) {
     ball.updatePostion();
-}
 
+}
 int main(int argc, char *argv[]) {
 //    std::cout << "Hello, World!" << std::endl;
 
@@ -177,37 +199,52 @@ int main(int argc, char *argv[]) {
 
     Player player(PLAYER_START_POS_X, PLAYER_START_POS_Y, PLAYER_WIDTH, PLAYER_HEIGHT, PLAYER_SPEED);
     Ball ball(BALL_START_POS_X, BALL_START_POS_Y, BALL_SIZE, BALL_SPEED);
-    Brick bricks[BRICK_COLUMNS * BRICK_ROWS];
 
-    for ()
-    Brick brick(0,0, BRICK_WIDTH, BRICK_HEIGHT);
+//    std::vector<std::vector<Brick>> *bricks;
+//
+////     Tworzenie Cegieł
+//    for (int i = 0; i < BRICK_ROWS; ++i) {
+//        std::vector<Brick> row;
+//        for (int j = 0; j < BRICK_COLUMNS; ++j) {
+//            int brickWidth = (WINDOW_WIDTH / BRICK_COLUMNS) - HORIZONTAL_SPACE_BETWEEN_BRICKS;
+//            int space_from_screen_sides = (WINDOW_WIDTH - (BRICK_COLUMNS * brickWidth + (BRICK_COLUMNS - 1) * HORIZONTAL_SPACE_BETWEEN_BRICKS)) / 2;
+//
+//            int x = space_from_screen_sides + j * (brickWidth + HORIZONTAL_SPACE_BETWEEN_BRICKS);
+//            int y = SPACE_FROM_SCREEN_TOP + i * (BRICK_HEIGHT + VERTICAL_SPACE_BETWEEN_BRICKS);
+//
+//            row.push_back(Brick(x, y, brickWidth, BRICK_HEIGHT));
+//        }
+//        bricks.push_back(row);
+//    }
 
+    Brick bricks[BRICK_ROWS][BRICK_COLUMNS];
+//     Tworzenie Cegieł
+    for (int i = 0; i < BRICK_ROWS; ++i) {
+        for (int j = 0; j < BRICK_COLUMNS; ++j) {
+            int brickWidth = (WINDOW_WIDTH / BRICK_COLUMNS) - HORIZONTAL_SPACE_BETWEEN_BRICKS;
+            int space_from_screen_sides = (WINDOW_WIDTH - (BRICK_COLUMNS * brickWidth + (BRICK_COLUMNS - 1) * HORIZONTAL_SPACE_BETWEEN_BRICKS)) / 2;
 
-    void setBricks(int i) {
-        bricks[i].rect.x = (((i % COL) + 1) * SPACING) + ((i % COL) * bricks[i].rect.w) - (SPACING / 2);
-        bricks[i].rect.y = bricks[i].rect.h * 3 + (((i % ROW) + 1) * SPACING) + ((i % ROW) * bricks[i].rect.h) - (SPACING / 2);
+            int x = space_from_screen_sides + j * (brickWidth + HORIZONTAL_SPACE_BETWEEN_BRICKS);
+            int y = SPACE_FROM_SCREEN_TOP + i * (BRICK_HEIGHT + VERTICAL_SPACE_BETWEEN_BRICKS);
+
+            bricks[i][j] = Brick(x, y, brickWidth, BRICK_HEIGHT);
+        }
     }
 
     while (game_active) {
         Uint64 start = SDL_GetPerformanceCounter();
 
-
         handleEvents(game_active, player);
-        handleCollisions(WINDOW_WIDTH, WINDOW_HEIGHT, MAX_BALL_SIDEWAYS_BOUNCE_FROM_PLAYER_ANGLE,
-                         MIN_BALL_UP_BOUNCE_FROM_PLAYER_ANGLE, player, ball);
+        handleCollisions(WINDOW_WIDTH, WINDOW_HEIGHT, MAX_BALL_SIDEWAYS_BOUNCE_OF_PLAYER, player, ball, bricks);
         updateObjects(ball);
-
-
-        drawObjects(renderer, player, ball);
-
-
+        drawObjects(renderer, player, ball, bricks);
 
 
         // Cap framerate
         Uint64 end = SDL_GetPerformanceCounter();
         float elapsedMS = (end - start) / (float)SDL_GetPerformanceFrequency() * 1000.0f;
         SDL_Delay(floor(16.666f - elapsedMS));
-//        SDL_Delay(300);
+//        SDL_Delay(3000);
 //        game_active = false;
     }
 
